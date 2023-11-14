1ad7c957e2b0457f33bb811c241e106c74dedb72
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-11-14 11:53:21 +0200

    buttonUpdate

--- src/components/ContactsBook/ContactsList/StyledContactList.js
+++ src/components/ContactsBook/ContactsList/StyledContactList.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 export const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;  // Выравнивание по центру
+  align-items: center;  
   gap: 15px;
 `;
 
 export const StyledContactItem = styled.li`
   display: flex;
-  align-items: center;  // Выравнивание по центру
+  align-items: center;  
   gap: 25px;
   font-size: 18px;
   font-weight: 400;
 `;
 
 export const StyledButton = styled.button`
-  padding: 0 15px;
+  padding: 10px 15px;
   border: none;
   background-color: turquoise;
   border-radius: 4px;
